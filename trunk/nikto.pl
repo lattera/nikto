620b230927f0b2b69819a78e8aa9081e47f6ed7b
deity <deity@cb301202-1dc0-4f3e-a0a0-cd5d3132b0a9>
2010-05-27 10:44:32 +0000

    Fixes to parameters and new parameter for test to report after a number of tests
    git-svn-id: http://svn2.assembla.com/svn/Nikto_2@425 cb301202-1dc0-4f3e-a0a0-cd5d3132b0a9

--- trunk/nikto.pl
+++ trunk/nikto.pl
@@ -200,7 +200,7 @@ foreach my $mark (@MARKS) {
     my $time    = date_disp($mark->{'end_time'});
     my $elapsed = $mark->{'end_time'} - $mark->{'start_time'};
     nprint(
-        "+ $mark->{'total_checks'} items checked: $mark->{'total_errors'} error(s) and $mark->{'total_vulns'} item(s) reported on remote host"
+        "+ $NIKTO{'total_checks'} items checked: $mark->{'total_errors'} error(s) and $mark->{'total_vulns'} item(s) reported on remote host"
         );
     nprint("+ End Time:           $time ($elapsed seconds)");
     nprint($NIKTO{'DIV'});
