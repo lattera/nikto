fc97743fbfb2302c6a84f30b12bc236157e57e80
sullo <sullo@cb301202-1dc0-4f3e-a0a0-cd5d3132b0a9>
2010-08-13 02:59:59 +0000

    Support for #171, proxy via command line. Needs more testing.
    git-svn-id: http://svn2.assembla.com/svn/Nikto_2@510 cb301202-1dc0-4f3e-a0a0-cd5d3132b0a9

--- trunk/nikto.pl
+++ trunk/nikto.pl
@@ -194,7 +194,6 @@ foreach my $mark (@MARKS) {
             $mark->{'banner'} = "(no identification possible)";
         }
 
-        #add_vulnerability($mark, "Web server banner: " . $mark->{'banner'}, 0);
         add_vulnerability($mark,
                    "Server: $protocol://$mark->{'display_name'}:$mark->{'port'}\t$mark->{'banner'}",
                    0);
